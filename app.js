35a6d4d30b4b12df02f879bc6793c3ca6989bd14
Abhijith BS <vivekabi1717@gmail.com>
2024-09-04 12:09:54 +0530

    changes made on server root folder to clear Vercel deployment issues

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import apiRouter from './routes/index.js';
-import serverConfig from './config/serverConfig.js';
-import { dbConnect } from './config/dbConfig.js';
+import apiRouter from './src/routes/index.js';
+import serverConfig from './src/config/serverConfig.js';
+import { dbConnect } from './src/config/dbConfig.js';
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
