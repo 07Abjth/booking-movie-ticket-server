3cfc2e297a897b82a7217c8773e5b066a862d553
Abhijith BS <vivekabi1717@gmail.com>
2024-10-08 19:09:17 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: ['https://cineticketsbook.vercel.app', 'http://localhost:4000'],
+  origin: ['cineticketsbook-n3rh1p6n8-abhijith-bss-projects.vercel.app', 'http://localhost:4000'],
   credentials: true,
 }));
 
