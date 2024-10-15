a0fed911583726be02df505a21454e2b317a218a
Abhijith BS <vivekabi1717@gmail.com>
2024-10-15 17:23:40 +0530

    #	modified:   src/controllers/paymentControllers.js #	modified:   src/routes/v1/index.js #	modified:   src/routes/v1/paymentRoutes.js

--- src/routes/v1/paymentRoutes.js
+++ src/routes/v1/paymentRoutes.js
@@ -1,35 +1,36 @@
 import express from 'express';
-import {createPayment,
-    handlePaymentResponse,
-    paymentSuccess,
-    paymentFailure,
-    getPayUHash
- } from '../../controllers/paymentControllers.js';
+
+
+// import {createPayment,
+//     handlePaymentResponse,
+//     paymentSuccess,
+//     paymentFailure,
+//     getPayUHash
+//  } from '../../controllers/paymentControllers.js';
 
 
 
-const router = express.Router();
+// const router = express.Router();
 
 
-// Route to create a payment
-router.post('/create-payment', createPayment);
+// // Route to create a payment
+// router.post('/create-payment', createPayment);
 
-// Route to handle the payment response from PayU
-router.post('/payment-response', handlePaymentResponse);
+// // Route to handle the payment response from PayU
+// router.post('/payment-response', handlePaymentResponse);
 
-// Success callback route
-router.post('/payment-success', paymentSuccess);
+// // Success callback route
+// router.post('/payment-success', paymentSuccess);
 
-// Failure callback route
-router.post('/payment-failure', paymentFailure);
-
-
-// Route to get the PayU hash
-router.post('/getPayUHash', getPayUHash);
+// // Failure callback route
+// router.post('/payment-failure', paymentFailure);
 
 
-export default router;
+// // Route to get the PayU hash
+// router.post('/getPayUHash', getPayUHash);
 
+
+ 
 
 
 
